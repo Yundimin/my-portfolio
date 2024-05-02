dcf370818e42f0175ba29b763271a8b05e6d9292
jimin yun <ge72946@gmail.com>
2024-05-02 23:49:16 +0900

    background Img scroll

--- src/pages/Home/Logo.tsx
+++ src/pages/Home/Logo.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import {
   DarkOverlay,
   LogoContent,
@@ -8,8 +9,29 @@ import textImg from "../../assets/text-rotate.png";
 import nameImg from "../../assets/CWJ.png";
 
 function Logo() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const backgroundImageSize = 100 + scrollPosition * 0.1 + "%";
+
   return (
-    <LogoWrapper>
+    <LogoWrapper
+      style={{
+        backgroundSize: backgroundImageSize,
+      }}
+    >
       <LogoContent>
         <img src={textImg} alt="textImg" className="item" />
       </LogoContent>
