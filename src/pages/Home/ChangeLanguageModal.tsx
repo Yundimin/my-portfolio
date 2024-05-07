b26a8e8110ed5281037caa788824c2e9e87fe5d5
jimin yun <ge72946@gmail.com>
2024-05-07 19:43:11 +0900

    change language function

--- src/pages/Home/ChangeLanguageModal.tsx
+++ src/pages/Home/ChangeLanguageModal.tsx
@@ -1,22 +1,45 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import i18n from "../../locales/i18n";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Form } from "react-bootstrap";
+
+interface LanguageChecked {
+  en: boolean;
+  ko: boolean;
+}
 
 function ChangeLanguageModal() {
   const [show, setShow] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+  const [languagesChecked, setLanguagesChecked] = useState<LanguageChecked>({
+    en: i18n.language === "en",
+    ko: i18n.language === "ko",
+  });
 
   const handleLanguageChange = (lang: string) => {
     setSelectedLanguage(lang);
     i18n.changeLanguage(lang);
+    setShow(false);
   };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleLanguageToggle = (lang: keyof LanguageChecked) => {
+    setLanguagesChecked({
+      en: lang === "en" ? !languagesChecked.en : false,
+      ko: lang === "ko" ? !languagesChecked.ko : false,
+    });
+    if (languagesChecked[lang]) {
+      setSelectedLanguage("");
+    } else {
+      setSelectedLanguage(lang);
+    }
+  };
 
   return (
     <>
@@ -29,20 +52,38 @@ function ChangeLanguageModal() {
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
+        centered
       >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+        <Modal.Header>
+          <Modal.Title>Language Change</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Do not even try to press
-          escape key.
+          <Form>
+            <Form.Check
+              type="checkbox"
+              id="en"
+              label="영어"
+              checked={languagesChecked["en"]}
+              onChange={() => handleLanguageToggle("en")}
+            />
+            <Form.Check
+              type="checkbox"
+              id="ko"
+              label="한국어"
+              checked={languagesChecked["ko"]}
+              onChange={() => handleLanguageToggle("ko")}
+            />
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => handleLanguageChange("en")}>
-            Understood
+          <Button
+            variant="primary"
+            onClick={() => handleLanguageChange(selectedLanguage)}
+          >
+            Change
           </Button>
         </Modal.Footer>
       </Modal>
--- src/styles/Headers.modules.ts
+++ src/styles/Headers.modules.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const NavbarWrapper = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   .navLinks {
